9048d161eef81cf3b091495eaae76d7a4d2a48ea
Namandeep Singh <109947466+namandeep463@users.noreply.github.com>
2022-07-26 13:29:18 +0530

    Create TestOOPs.java

---
+++ Test.java
@@ -0,0 +1,48 @@
+//Q.no 1
+class Test{
+
+   	public static void main(){
+		test obj= new Test();
+      obj.hobby();		
+	
+	}
+	void hobby(){
+	   String name= "Naman Bhardwaj"
+	   int age=21;
+	   System.out.println("name="+name );
+	   System.out.println("age="+age );
+	}
+	
+}
+//Q.no 2
+//Ans
+//Class Name- Test
+//Function Name-void main()
+//Variable-Test
+//Static Variable-TEST
+
+//Q.no 5
+//Ans
+//These classes are Wrapped around the primitive data to provide function of object oriented programming.
+//eg. integer class has wrapper class of int data type.
+//i.e. integer int=i;
+
+
+//Q.no 3
+//Ans-1 java file has all the code written by the programmer.This is code before compilation.
+//Ans-2 java is not complete OOP'S programming language.java 
+//contains permitive value like int,char which are not object. It contain correspenting
+//wrapper classes to its primitive values.Skill primitive value exist is not a complete OOP'S programming
+//Ans-3 
+//void add(int 1, int 2){
+//body
+//}
+	
+
+
+
+
+ 
+
+
+
---
+++ TestOOPs.java
@@ -0,0 +1,16 @@
+class TestOOPs {
+	
+	public static void main(String[] a) {
+	
+	int i2 = 10;
+		
+		
+		//Integer i2 = new Integer(20); 
+		//Wrapper Classes
+		  //i2 = 20;
+		
+		System.out.print("Value of object = " + i2);
+	
+	}
+
+}
