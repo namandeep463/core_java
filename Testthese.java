cd6d1f7e1275e9d8c9cf77a05bd019b3f28e6e5c
Namandeep Singh <109947466+namandeep463@users.noreply.github.com>
2022-08-02 14:01:15 +0530

    Create Testthese.java
    Assignment no 2

---
+++ Testthese.java
@@ -0,0 +1,115 @@
+//Q.no 5
+//Ans
+//pass by value
+// in the technique the copy of the variable is passed the method. if the value ofthe variable is change inside to the
+//variable 
+
+//pass by reference 
+//the technique we pass the whole reference  of the variable instread of passing its copy to the method .if we make 
+
+//Q.no 1
+import java.util.Scanner;
+class Naman{
+	public static void main(String []arg){
+	System.out.print("Print a number the check if the number is even or odd");
+	Scanner obj = new Scanner(System.in);
+	int m= obj.nextInt();
+	
+	if(m%2==0)
+		System.out.println(m+"is even");
+	else
+		System.out.println(m+"is odd");
+	}
+	
+}
+
+//Q.NO 7
+import java.util.Scanner;
+  class Copyrights{
+	public static void main(String[] args){
+		int ar[] = new int [10];
+		Scanner sc = new Scanner(System.in)
+        for(int i= 0;i < 10;i++){
+         System.out.println("enter the index"+i+"");
+          int input = sc.nextint();
+		  ar[i]=input;
+		}
+		System.out.println("array is");
+		for(int i = 0;i < 10;i++){
+			
+			 System.out.print("ar[i]+");
+		  
+		}
+		
+	}
+	
+//Q.no 2
+import java.util.Scanner
+class Mocktest{
+     public static void main(String[] agrs){
+		 Scanner.sc = new Scanner(System.in);
+		 System.out.println("enter the number");
+		 int input = sc.nextint();
+		 if(input>60){
+			 System.out,println("The number is greater than 60");
+		 }
+		 else{
+			 System.out.println("the number is smaller than 60");
+			 
+		 }
+	 }	 
+		 
+//Q.no 3
+import java.util.Scanner;
+ class Naman{
+    public static void main(String[] args){
+   Scanner.sc = new Scanner(System.in);
+    System.out.println("enter the number 1");
+	int in1 = sc.nextint();
+	System.out.println("enter the number 2");
+	int in2 = sc.nextint();
+	if(in1>in2){
+		System.out.println(in1"is greater"in2);
+	}
+	else(in1<in2){
+		System.out.println(in1"is smaller"in2);
+	}
+	else if(in1==){
+		System.out.println(in1"is equals"in2);
+	}
+	
+	}
+ }
+ 
+//Q.no 4
+ class Test{
+     public static void main(String[] args){
+         Scanner.sc = new Scanner(System.in);
+         System.out.print("enter the number")
+         int in1 = sc.nextint();
+       switch(in1){
+      case(579): System.out.println(in1"this is soun roll no");
+	     break;
+	  case(546): System.out.println(in1"this is monu roll no"); 
+	          break;
+	    case(553): System.out.println(in1"this is manu roll no"); 
+	             break;
+	    case(544): System.out.println(in1"this is meenu roll no"); 
+	         break;
+	   case(516): System.out.println(in1"this is meena roll no"); 
+	      break;
+	
+	
+	
+	
+
+
+	
+
+
+
+
+
+
+
+ 
